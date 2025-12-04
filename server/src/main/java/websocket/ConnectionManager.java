c641ea1d66b3b479bd4c8b5fa429550710e41ef7
RachelrWhite <rachelrwhite04@gmail.com>
2025-12-03 23:47:51 -0700

    made some changes and added the getInfo and broadcastToGame functions in teh ConnectionManager

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,4 +1,5 @@
 package websocket;
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.*;
 
@@ -8,8 +9,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final HashMap<Session, GameInfo> connections = new HashMap<>();
 
-
-
     public void add(Session session, GameInfo info) {
         connections.put(session, info);
     }
@@ -18,14 +17,20 @@ public class ConnectionManager {
         connections.remove(session);
     }
 
-    public void broadcast(Session excludeSession, ServerMessage message) throws IOException {
-        String msg = message.toString();
-        for (Session c : connections.values()) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
-                }
-            }
+    public GameInfo getInfo(Session session) {
+        return connections.get(session);
+    }
+
+    // this one is going to broadcast to every player in the game
+    public void broadcastToGame(int gameID, ServerMessage message, Session exclude) throws IOException {
+        String json = new Gson().toJson(message);
+        for (var entry : connections.entrySet()) {
+            var session = entry.getKey();
+            var info = entry.getValue();
+            if (!session.isOpen()) continue;
+            if (info.gameID() != gameID) continue;
+            if (exclude != null && session.equals(exclude)) continue;
+            session.getRemote().sendString(json);
         }
     }
 }
